c3aef07acfc099ccdfdff5a0dccca15ca564e631
Benjamin Wollenweber <ich@wollis-page.de>
2021-01-04 17:47:10 +0100

    Increase sleep times

--- src/hcServer.py
+++ src/hcServer.py
@@ -112,7 +112,7 @@ class HcServer:
                         except:
                             pass
 
-                sleep(.001)
+                sleep(.01)
 
     def scan_bus(self):
         self.logger.info("Scan bus")
@@ -140,7 +140,7 @@ class HcServer:
             except:
                 self.slaves[address].set_active(False)
 
-            sleep(.001)
+            sleep(.01)
 
         self.scanInProcess = False
         self.logger.info("Bus scanned")
